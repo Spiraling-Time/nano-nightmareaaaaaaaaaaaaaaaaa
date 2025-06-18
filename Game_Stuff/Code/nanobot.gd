3f0c3fa3a6fda4e7be162db0b04dbbfd363202e3
Spiraling-Time <yairmeirnoam.peck@gmail.com>
2025-06-18 19:18:34 -0400

    beginning to make stuff make sense

--- Game_Stuff/Code/nanobot.gd
+++ Game_Stuff/Code/nanobot.gd
@@ -2,6 +2,10 @@ extends CharacterBody2D
 
 var nano_type
 var nano_home
+var nano_size_x
+var nano_size_y
+
+
 
 var direction = Vector2.ZERO
 
@@ -9,35 +13,50 @@ var speed = 100
 
 var mode = "AWAY"
 
+func _ready() -> void:
+	move_randomly()
+
 func _physics_process(delta: float) -> void:
-	if nano_type == "TORSO":
-		if mode == "AWAY":
-			var decide_direction = randi_range(1,8)
-			if decide_direction == 1:
-				direction = Vector2(1,1)
-			if decide_direction == 2:
-				direction = Vector2(-1,1)
-			if decide_direction == 3:
-				direction = Vector2(1,-1)					
-			if decide_direction == 4:
-				direction = Vector2(-1,-1)
-			if decide_direction == 5:
-				direction = Vector2(-1,0)
-			if decide_direction == 6:
-				direction = Vector2(1,0)
-			if decide_direction == 7:
-				direction = Vector2(0,-1)
-			if decide_direction == 8:
-				direction = Vector2(0,1)
-		elif mode == "RETURN":
-			var point_towards = nano_home - global_position
-			direction = point_towards.normalized()
+	if mode == "RETURN":
+		var point_towards = nano_home - global_position
+		direction = point_towards.normalized()
 	
 	
 	velocity = direction * speed
 	move_and_slide()
-	if position.distance_to(nano_home) > 50:
+	if position.x > nano_home.x + nano_size_x or position.x < nano_home.x - nano_size_x or position.y > nano_home.y + nano_size_y or position.y < nano_home.y - nano_size_y:
 		mode = "RETURN"
 	if position.distance_to(nano_home) < 10:
 		mode = "AWAY"
+		move_randomly()
+		
 	print(mode)
+
+
+func move_randomly():
+	#if randi_range(1, 10) != 0:
+	var decide_direction = randi_range(1,8)
+	if decide_direction == 1:
+		direction = Vector2(1,1)
+	if decide_direction == 2:
+		direction = Vector2(-1,1)
+	if decide_direction == 3:
+		direction = Vector2(1,-1)					
+	if decide_direction == 4:
+		direction = Vector2(-1,-1)
+	if decide_direction == 5:
+		direction = Vector2(-1,0)
+	if decide_direction == 6:
+		direction = Vector2(1,0)
+	if decide_direction == 7:
+		direction = Vector2(0,-1)
+	if decide_direction == 8:
+		direction = Vector2(0,1)
+	#else:
+		#var point_towards = nano_home - global_position
+		#direction = -1 * point_towards.normalized()
+			
+			
+func _on_timer_timeout() -> void:
+	if mode == "AWAY":
+		move_randomly()
--- Game_Stuff/Code/world.gd
+++ Game_Stuff/Code/world.gd
@@ -6,7 +6,7 @@ var number_of_bots = 0
 
 func _ready() -> void:
 	randomize()
-	spawn_bots(100)
+	spawn_bots(145)
 
 func _physics_process(delta: float) -> void:
 	pass
@@ -14,13 +14,15 @@ func _physics_process(delta: float) -> void:
 	
 func spawn_bots(count: int):
 	for i in count:
-		if number_of_bots < 200:
+		if number_of_bots < 146:
 			number_of_bots += 1
 			var new_bot = bot.instantiate()
-			if number_of_bots <= 100:
+			if number_of_bots <= 145:
 				new_bot.nano_type = "TORSO"
-				new_bot.nano_home = $human_body/body/torso.position
-				new_bot.position = $human_body/body/torso.position + Vector2(randi_range(-10,10),randi_range(-10,10))
+				new_bot.nano_home = $human_body/body/torso.global_position
+				new_bot.nano_size_x = 75
+				new_bot.nano_size_y = 200
+				new_bot.position = $human_body/body/torso.global_position + Vector2(randi_range(-10,10),randi_range(-10,10))
 			
 			
 			
