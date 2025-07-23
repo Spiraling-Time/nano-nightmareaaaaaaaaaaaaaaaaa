b96d05b9d4af6a62591ce00d296e6a265ea72ec6
Spiraling-Time <yairmeirnoam.peck@gmail.com>
2025-07-23 15:24:47 -0400

    Add modular robo legs to player and refactor movement
    Replaced the player node from CharacterBody2D to RigidBody2D and removed legacy movement code. Introduced modular robo legs as separate scenes and scripts, attached via PinJoint2D, with their own input handling and attack logic. Updated player and world scenes to support the new structure. Also made minor fixes in the godot_super-wakatime plugin and removed web export files.

--- Game_Stuff/Code/player.gd
+++ Game_Stuff/Code/player.gd
@@ -1,4 +1,4 @@
-extends CharacterBody2D
+extends RigidBody2D
 
 var speed = 1000
 var gravity = 0
@@ -7,41 +7,5 @@ var fake_scale = 1
 var jump_height = 1000
 
 func _ready() -> void:
-	pass
-
-func _physics_process(delta: float) -> void:
-	pass
-			#if $attack1.get_overlapping_bodies().size() > 0:
-				#for bodies in $attack1.get_overlapping_bodies():
-					#if bodies != self and bodies.has_method("delete_self"):
-						#$"..".max_number_of_bots -= 1
-						# FIX THIS $"..".number_of_bots -= 1
-						#bodies.delete_self()
-	
-
-	
-	if Input.get_action_strength("left") >= 1:
-		velocity.x = -1 * speed
-		if fake_scale != -1:
-			scale.x = -1
-			fake_scale = -1
-	elif Input.get_action_strength("right") >= 1:
-		velocity.x = speed
-		if fake_scale != 1:
-			scale.x = -1
-			fake_scale = 1
-
-	else: velocity.x = 0
-	
-	if Input.get_action_strength("up") >= 1 and $RayCast2D.is_colliding():
-		gravity -= jump_height
-	
-	velocity.y += gravity
-	if gravity < 0: gravity += 10
-	elif !$RayCast2D.is_colliding():
-		gravity += 1
-		if gravity < 0: gravity = gravity/10
-		
-	move_and_slide()
-	if $RayCast2D.is_colliding():
-		gravity = 0
+	$Robo_leg1.leg_number = 1
+	$Robo_leg2.leg_number = 2
---
+++ Game_Stuff/Code/robo_leg.gd
@@ -0,0 +1,71 @@
+extends RigidBody2D
+
+@onready var ani = $AnimationPlayer
+
+var power = 500
+
+var leg_number = 0
+
+var pin
+
+func _ready() -> void:
+	ani.play("Idle")
+	if leg_number == 1: pin = $"../PinJoint2D1"
+	elif leg_number == 2: pin = $"../PinJoint2D2"
+
+func _physics_process(delta: float) -> void:
+	if leg_number == 1:
+		if Input.get_action_strength("front_left") >=1:
+			
+			apply_torque_impulse(power*-1)
+			$"../Sprite2D".flip_h = true
+		elif Input.get_action_strength("front_right") >=1:
+			apply_torque_impulse(power)
+			$"../Sprite2D".flip_h = false
+		
+		if Input.get_action_strength("front_up") >=1:
+			ani.play("Fly")
+			#$"../PinJoint2D1".angular_limit_enabled = true
+			pin.set_angular_limit_lower(rotation)
+			pin.set_angular_limit_upper(rotation)
+			linear_velocity = Vector2.UP.rotated(rotation) * power/10
+		else:
+			#$"../PinJoint2D1".angular_limit_enabled = false
+			ani.play("Idle")
+			
+	elif leg_number == 2:
+		if Input.get_action_strength("back_left") >=1:
+			apply_torque_impulse(power*-1)
+			$"../Sprite2D".flip_h = true
+		elif Input.get_action_strength("back_left") >=1:
+			apply_torque_impulse(power)
+			$"../Sprite2D".flip_h = false
+		
+		if Input.get_action_strength("back_up") >=1:
+			ani.play("Fly")
+			#$"../PinJoint2D2".angular_limit_enabled = true
+			pin.set_angular_limit_lower(rotation)
+			pin.set_angular_limit_upper(rotation)
+			linear_velocity = Vector2.UP.rotated(rotation) * power/10
+		else:
+			#$"../PinJoint2D2".angular_limit_enabled = false
+			ani.play("Idle")
+	
+	
+	if ani.current_animation == "Fly":
+		if $Area2D.get_overlapping_bodies().size() > 0:
+			for bodies in $Area2D.get_overlapping_bodies():
+				if bodies != self and bodies.has_method("delete_self"):
+					#$"..".max_number_of_bots -= 1
+					bodies.get_parent().number_of_bots -= 1
+					# FIX THIS $"..".number_of_bots -= 1
+					bodies.delete_self()
+	
+
+
+func burn_active():
+	$Area2D/CollisionShape2D.disabled = false
+	
+	
+func burn_deactivated():
+	$Area2D/CollisionShape2D.disabled = true
