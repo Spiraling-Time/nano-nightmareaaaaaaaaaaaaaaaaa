987be1e76f51bf32298f39407c81c6ad0630bf32
Spiraling-Time <yairmeirnoam.peck@gmail.com>
2025-07-23 22:45:38 -0400

    Improve robo leg flight control and add stop input
    Enhanced robo_leg.gd to use gradual flight power and reset on stop input. Updated player.gd to fix position reference and added commented gravity/rotation controls. Adjusted PinJoint2D settings in player.tscn for better leg movement. Modified robo_leg.tscn collision shape size and position for improved hitbox. Added 'stop' input action to project.godot.

--- Game_Stuff/Code/player.gd
+++ Game_Stuff/Code/player.gd
@@ -11,7 +11,11 @@ func _ready() -> void:
 	$Robo_leg2.leg_number = 2
 
 func _physics_process(delta: float) -> void:
-	if global_position.x > $"../human_body".position.x: $Sprite2D.flip_h = true
+	if global_position.x > $"../human_body".global_position.x: $Sprite2D.flip_h = true
 	else: $Sprite2D.flip_h = false
-	
-	#angular_velocity = rotation *-1
+	#if Input.get_action_strength("gravity_shift") >= 1:
+		#PhysicsServer2D.area_set_param(get_viewport().find_world_2d().space, PhysicsServer2D.AREA_PARAM_GRAVITY_VECTOR, (global_position.direction_to($"../human_body".global_position)).normalized())
+	#if Input.get_action_strength("front_left") >= 1 or Input.get_action_strength("back_left") >= 1:
+		#angular_velocity = -1
+	#elif Input.get_action_strength("front_right") >= 1 or Input.get_action_strength("back_right") >= 1:
+		#angular_velocity = 1
--- Game_Stuff/Code/robo_leg.gd
+++ Game_Stuff/Code/robo_leg.gd
@@ -2,8 +2,10 @@ extends RigidBody2D
 
 @onready var ani = $AnimationPlayer
 
-var power = 20#500
-var flight_power = 300
+var power = 20 #500
+var max_flight_power = 500
+
+var leg_flight_power = 0
 
 var leg_number = 0
 
@@ -16,39 +18,57 @@ func _ready() -> void:
 	if leg_number == 1: pin = $"../PinJoint2D1"
 	elif leg_number == 2: pin = $"../PinJoint2D2"
 
-func _physics_process(delta: float) -> void:
+
+
+func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
+	if Input.get_action_strength("stop") >=1:
+		#power = 40
+		leg_flight_power = 0
+	#else: power = 20
 	
 
-	
 	if leg_number == 1:
 		if Input.get_action_strength("front_left") >=1:
 			angular_velocity = -1*power
 			#apply_torque_impulse(power*-1)
+			#apply_torque(power)
 		elif Input.get_action_strength("front_right") >=1:
 			angular_velocity = power
 			#apply_torque_impulse(power)
+			#apply_torque(power)
 		
 		if Input.get_action_strength("front_up") >=1:
+			leg_flight_power = leg_flight_power*1.1 + .001
+			if leg_flight_power > max_flight_power: leg_flight_power = max_flight_power
 			ani.play("Fly")
-			linear_velocity = Vector2.UP.rotated(rotation) * flight_power
+			if leg_flight_power > 1: linear_velocity = Vector2.UP.rotated(rotation) * leg_flight_power
+			#apply_central_force(Vector2.UP.rotated(rotation)*flight_power)
 		else:
 			#$"../PinJoint2D1".angular_limit_enabled = false
+			leg_flight_power = 0
 			ani.play("Idle")
-			
+		
 	elif leg_number == 2:
 		if Input.get_action_strength("back_left") >=1:
 			angular_velocity =-1* power
 			#apply_torque_impulse(power*-1)
+			#apply_torque(power)
 		elif Input.get_action_strength("back_right") >=1:
 			angular_velocity = power
 			#apply_torque_impulse(power)
+			#apply_torque(power)
 		
 		if Input.get_action_strength("back_up") >=1:
+			leg_flight_power = leg_flight_power*1.1 + .001
+			if leg_flight_power > max_flight_power: leg_flight_power = max_flight_power
 			ani.play("Fly")
-			linear_velocity = Vector2.UP.rotated(rotation) * flight_power
+			if leg_flight_power > 1: linear_velocity = Vector2.UP.rotated(rotation) * leg_flight_power
+			#apply_central_force(Vector2.UP.rotated(rotation)*flight_power)
 		else:
+			leg_flight_power = 0
 			ani.play("Idle")
 	
+
 	
 	if ani.current_animation == "Fly":
 		if $Area2D.get_overlapping_bodies().size() > 0:
@@ -58,8 +78,8 @@ func _physics_process(delta: float) -> void:
 					bodies.get_parent().number_of_bots -= 1
 					# FIX THIS $"..".number_of_bots -= 1
 					bodies.delete_self()
-
-
+	
+	
 func burn_active():
 	$Area2D/CollisionShape2D.disabled = false
 	
