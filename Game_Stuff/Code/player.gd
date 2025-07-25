5dd5972c709f95900fd2046cedeec595e8c92318
Spiraling-Time <yairmeirnoam.peck@gmail.com>
2025-07-25 13:04:27 -0400

    no more sleeping! Flight controls work!

--- Game_Stuff/Code/player.gd
+++ Game_Stuff/Code/player.gd
@@ -11,6 +11,13 @@ var flight_power = 20
 var max_turn_power = 1
 var turn_power = max_turn_power
 
+var left_pressed: bool = false
+var right_pressed: bool = false
+
+var spin_left : bool = false
+var spin_right : bool = false
+
+
 func _ready() -> void:
 
 	$AnimationPlayer.play("Idle")
@@ -29,9 +36,9 @@ func _physics_process(delta: float) -> void:
 
 func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
 	
-	if Input.get_action_strength("spin") >= .1:
-		turn_power = max_turn_power * 20
-	else: turn_power = max_turn_power
+	#if Input.get_action_strength("spin") >= .1:
+		#turn_power = max_turn_power * 20
+	#else: turn_power = max_turn_power
 	
 
 	if Input.get_action_strength("up") >= .1:
@@ -41,7 +48,36 @@ func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
 		$AnimationPlayer.play("Idle")
 
 	if Input.get_action_strength("left") >= .1:
-		angular_velocity = -1*turn_power
-
+		if spin_left:
+			left_pressed = true
+			angular_velocity = -20*turn_power
+		else:
+			left_pressed = true
+			angular_velocity = -1*turn_power
+	
 	elif Input.get_action_strength("right") >= .1:
-		angular_velocity = turn_power
+		if spin_right:
+			right_pressed = true
+			angular_velocity = 20*turn_power
+		else:
+			right_pressed = true
+			angular_velocity = turn_power
+	
+	if Input.get_action_strength("left") <= 0:
+		if left_pressed:
+			left_pressed = false
+			spin_left = false
+			$left_timer.start()
+
+	if Input.get_action_strength("right") <= 0:
+		if right_pressed:
+			right_pressed = false
+			spin_right = false
+			$right_timer.start()
+
+
+func _on_left_timer_timeout() -> void:
+	if left_pressed: spin_left = true
+
+func _on_right_timer_timeout() -> void:
+	if right_pressed: spin_right = true
