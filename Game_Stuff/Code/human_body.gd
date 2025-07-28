3e544cc15287077eb086afddc23f07f84b38ac56
Spiraling-Time <yairmeirnoam.peck@gmail.com>
2025-07-28 14:14:37 -0400

    I got working legs! TIME TO DO IT AGAIN UNTIL IT IS PERFECT
    Replaced walk_type with leg1_movement and leg2_movement for more granular control of leg animation. Updated walking logic to use new movement states and added a Reset_height timer for leg position resets. Updated scene files to reflect these changes and removed unused animation timer and signal connection.

--- Game_Stuff/Code/human_body.gd
+++ Game_Stuff/Code/human_body.gd
@@ -17,6 +17,8 @@ extends Node2D
 @onready var near_wall1 = $body/torso/near_wall1
 @onready var near_wall2 = $body/torso/near_wall2
 
+@onready var leg_height_resetter = $Reset_height
+
 var max_speed = 10
 var speed = max_speed
 
@@ -27,7 +29,8 @@ var mode = "WALK"
 var fall_speed = 1
 
 
-var walk_type:bool = false
+var leg1_movement = "Forward"
+var leg2_movement = "Backward"
 
 
 var distribution: Array = []
@@ -35,7 +38,7 @@ var distribution: Array = []
 var facing = "left"
 
 func _ready() -> void:
-	basic_position()
+	reset_basic_position()
 	basic_rotation()
 
 func _physics_process(delta: float) -> void:
@@ -49,43 +52,74 @@ func _physics_process(delta: float) -> void:
 			
 	#print("nano: ", global_position.x, " player: ", $"../player".global_position.x)
 	if mode == "WALK":
-		if $Animation_Timer.is_stopped():
-			walk_type = !walk_type
-			$Animation_Timer.start()
+		if leg_height_resetter.is_stopped():
+			reset_basic_position()
+			leg_height_resetter.start()
 		if speed > 0:
 			if !near_wall1.is_colliding():
 				position.x += speed
 		elif !near_wall2.is_colliding():
 			position.x += speed
-		basic_position()
-		if !walk_type:
+		basic_positions(false, false, false, false, false, false, false, true, false, true)
+		if leg1_movement == "Forward":
 			if facing == "left":
-				upper_leg1.rotation += 0.1
-				upper_leg2.rotation += -0.1
-				
+				upper_leg1.rotation += 0.2
+				#upper_leg2.rotation += -0.1
+				#print(upper_leg1.rotation)
+				if upper_leg1.rotation >= 2:
+					leg1_movement = "Backward"
+					upper_leg1.position.y -= 10
 			else:
-				upper_leg1.rotation += -0.1
-				upper_leg2.rotation += 0.1
-		else:
-			if facing == "left":
-				upper_leg1.rotation += -0.1
-				if upper_leg1.rotation == 0:
-					walk_type = !walk_type
-					$Animation_Timer.start()
-				upper_leg2.rotation += 0.1
-				if upper_leg2.rotation == 0:
-					walk_type = !walk_type
-					$Animation_Timer.start()
+				upper_leg1.rotation += -0.2
+				#upper_leg2.rotation += 0.1
+				if upper_leg1.rotation <= -0.6:
+					leg1_movement = "Backward"
+					upper_leg1.position.y += 10
 
+		elif leg1_movement == "Backward":
+			if facing == "left":
+				upper_leg1.rotation += -0.2
+				#upper_leg2.rotation += 0.1
+				if upper_leg1.rotation <= -0.6:
+					leg1_movement = "Forward"
+					upper_leg1.position.y += 10
 			else:
-				upper_leg1.rotation += 0.1
-				if upper_leg1.rotation == 0:
-					walk_type = !walk_type
-					$Animation_Timer.start()
-				upper_leg2.rotation += -0.1
-				if upper_leg2.rotation == 0:
-					walk_type = !walk_type
-					$Animation_Timer.start()			
+				upper_leg1.rotation += 0.2
+				#upper_leg2.rotation += -0.1
+				if upper_leg1.rotation >= 2:
+					leg1_movement = "Forward"
+					upper_leg1.position.y -= 10
+		if leg2_movement == "Forward":
+			if facing == "left":
+				
+				upper_leg2.rotation += 0.2
+				#upper_leg2.rotation += -0.1
+				#print(upper_leg1.rotation)
+				if upper_leg2.rotation >= 0.6:
+					leg2_movement = "Backward"
+					upper_leg2.position.y += 10
+			else:
+				upper_leg2.rotation += -0.2
+				#upper_leg2.rotation += 0.1
+				if upper_leg2.rotation <= -2:
+					leg2_movement = "Backward"
+					upper_leg2.position.y -= 10
+		elif leg2_movement == "Backward":
+			if facing == "left":
+				upper_leg2.rotation += -0.2
+				#upper_leg2.rotation += 0.1
+				if upper_leg2.rotation <= -2:
+					leg2_movement = "Forward"
+					upper_leg2.position.y -= 10
+			else:
+				upper_leg2.rotation += 0.2
+				#upper_leg2.rotation += -0.1
+				if upper_leg2.rotation >= 0.6:
+					leg2_movement = "Forward"
+					upper_leg2.position.y += 10
+					
+		print("leg1_movement: ", leg1_movement, " leg2_movement: ", leg2_movement)
+		print("leg1_rotation: ", upper_leg1.rotation, " leg2_rotation: ", upper_leg2.rotation)
 			
 			
 			
@@ -98,62 +132,58 @@ func _physics_process(delta: float) -> void:
 			position.y -= fall_speed
 			fall_speed = 1
 	if position.y >= -446.363: position.y = -446.363
-	
-func basic_position():
+
+func reset_basic_position():
+	basic_positions(true, true, true, true, true, true, true, true, true, true)
+
+func basic_positions(a: bool, b: bool, c: bool, d: bool, e: bool, f: bool, g: bool, h: bool, i: bool, j: bool):
 	#position.y = -446.363
 	if facing == "left":
-		torso.position = Vector2(25.0, 232.0)
-		head.position = Vector2(-1.0, -297.0)
-		upper_arm1.position = Vector2(-189.0, -148.0)
-		lower_arm1.position = Vector2(-200.0, 26.0)
-		upper_arm2.position = Vector2(139.0, -148.0)
-		lower_arm2.position = Vector2(200.0, 26.0)
-		upper_leg1.position = Vector2(-109.0, 260.0)
-		lower_leg1.position = Vector2(-94.0, 168.0)
-		upper_leg2.position = Vector2(139.0, 260.0)
-		lower_leg2.position = Vector2(-32.00, 174.0)
+		if a: torso.position = Vector2(25.0, 232.0)
+		if b: head.position = Vector2(-1.0, -297.0)
+		if c: upper_arm1.position = Vector2(-189.0, -148.0)
+		if d: lower_arm1.position = Vector2(-200.0, 26.0)
+		if e: upper_arm2.position = Vector2(139.0, -148.0)
+		if f: lower_arm2.position = Vector2(200.0, 26.0)
+		if g: upper_leg1.position = Vector2(-109.0, 260.0)
+		if h: lower_leg1.position = Vector2(-94.0, 168.0)
+		if i: upper_leg2.position = Vector2(139.0, 260.0)
+		if j: lower_leg2.position = Vector2(-32.00, 174.0)
 	else:
-		torso.position = Vector2(-25.0, 232.0)
-		head.position = Vector2(1.0, -297.0)
-		upper_arm1.position = Vector2(189.0, -148.0)
-		lower_arm1.position = Vector2(200.0, 26.0)
-		upper_arm2.position = Vector2(-139.0, -148.0)
-		lower_arm2.position = Vector2(-200.0, 26.0)
-		upper_leg1.position = Vector2(109.0, 260.0)
-		lower_leg1.position = Vector2(94.0, 168.0)
-		upper_leg2.position = Vector2(-139.0, 260.0)
-		lower_leg2.position = Vector2(32.00, 174.0)	
+		if a: torso.position = Vector2(-25.0, 232.0)
+		if b: head.position = Vector2(1.0, -297.0)
+		if c: upper_arm1.position = Vector2(189.0, -148.0)
+		if d: lower_arm1.position = Vector2(200.0, 26.0)
+		if e: upper_arm2.position = Vector2(-139.0, -148.0)
+		if f: lower_arm2.position = Vector2(-200.0, 26.0)
+		if g: upper_leg1.position = Vector2(109.0, 260.0)
+		if h: lower_leg1.position = Vector2(94.0, 168.0)
+		if i: upper_leg2.position = Vector2(-139.0, 260.0)
+		if j: lower_leg2.position = Vector2(32.00, 174.0)	
 
 func basic_rotation():
 	upper_leg1.rotation = 0.0
 	upper_leg2.rotation = 0.0
 
-func walk_position1():
-	basic_position()
-	if facing == "left":
-		upper_leg1.rotation = 100
-		upper_leg2.rotation = -100
-		#upper_leg1.position = Vector2(-189.0, 160.0)
-		#lower_leg1.position = Vector2(-94.0, 168.0)
-		#upper_leg2.position = Vector2(195.0, 208.0)
-		#lower_leg2.position = Vector2(-32.00, 174.0)
-		
-	else:
-		upper_leg1.rotation = -100
-		upper_leg2.rotation = 100
-		#upper_leg1.position = Vector2(189.0, 160.0)
-		#lower_leg1.position = Vector2(94.0, 168.0)
-		#upper_leg2.position = Vector2(-195.0, 208.0)
-		#lower_leg2.position = Vector2(32.00, 174.0)
+#func walk_position1():
+	#basic_position()
+	#if facing == "left":
+		#upper_leg1.rotation = 100
+		#upper_leg2.rotation = -100
+		##upper_leg1.position = Vector2(-189.0, 160.0)
+		##lower_leg1.position = Vector2(-94.0, 168.0)
+		##upper_leg2.position = Vector2(195.0, 208.0)
+		##lower_leg2.position = Vector2(-32.00, 174.0)
+		#
+	#else:
+		#upper_leg1.rotation = -100
+		#upper_leg2.rotation = 100
+		##upper_leg1.position = Vector2(189.0, 160.0)
+		##lower_leg1.position = Vector2(94.0, 168.0)
+		##upper_leg2.position = Vector2(-195.0, 208.0)
+		##lower_leg2.position = Vector2(32.00, 174.0)
 
-func walk_position2():
-	basic_position()
-	upper_leg1.rotation = 0
-	upper_leg2.rotation = 0
-
-
-
-func _on_animation_timer_timeout() -> void:
-	#if !walk_type: walk_position1()
-	#else: walk_position2()
-	pass
+#func walk_position2():
+	#basic_position()
+	#upper_leg1.rotation = 0
+	#upper_leg2.rotation = 0
